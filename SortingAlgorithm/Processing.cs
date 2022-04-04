d087726df7d3e0b3d0cc228a8415e392bffccb1c
amyard <maksymturenko@gmail.com>
2022-04-04 22:02:41 +0300

    sort simple solution

---
+++ SortingAlgorithm/Processing.cs
@@ -0,0 +1,20 @@
+﻿namespace SortingAlgorithm;
+
+public class Processing
+{
+    public async Task Execute(Stream source, Stream target, CancellationToken token)
+    {
+        using var streamReader = new StreamReader(source);
+        var lines = new List<string>();
+        while (!streamReader.EndOfStream)
+        {
+            lines.Add(await streamReader.ReadLineAsync());
+        }
+
+        await using var streamWriter = new StreamWriter(target);
+        foreach (var line in lines.OrderBy(x => x))
+        {
+            await streamWriter.WriteLineAsync(line);
+        }
+    }
+}
---
+++ SortingAlgorithm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using SortingAlgorithm;
+
+var processing = new Processing();
+
+var unsorted = File.OpenRead(@"D:\rider_projects\test_api\generated_data\sorttest\date_1000.csv");
+var sorted = new FileStream(@"D:\rider_projects\test_api\generated_data\sorttest\Sorted.csv", FileMode.OpenOrCreate, FileAccess.Write);
+
+Console.WriteLine("Sorting process start.");
+var timer = new Stopwatch();
+timer.Start();
+
+await processing.Execute(unsorted, sorted, new CancellationToken()); 
+
+timer.Stop();
+TimeSpan timeTaken = timer.Elapsed;
+Console.WriteLine($"Sorting process finish. Time {timeTaken}");
+    
+    
