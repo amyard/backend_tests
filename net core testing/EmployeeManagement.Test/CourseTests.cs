354ae0ad2834f9fab22f3aeaef728084285c214c
Maksym <maksymturenko@gmail.com>
2022-07-13 19:28:59 +0300

    add some mvc tests.

---
+++ net core testing/EmployeeManagement.Test/CourseTests.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+using Xunit;
+
+namespace EmployeeManagement.Test;
+
+public class CourseTests
+{
+    [Fact]
+    public void CourseConstructor_ConstructCourse_IsNewMustBeTrue()
+    {
+        // Arrange
+        // Act
+        var course = new Course("New course");
+        
+        // Assert
+        Assert.True(course.IsNew);
+    }
+}
